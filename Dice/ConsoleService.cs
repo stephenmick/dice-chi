f732cf0d32fda9e76cce42a224a92889e43f02e2
Stephen Mick <stephen.mick@gmail.com>
2021-10-22 20:42:47 -0400

    added functionality for different sided dice

--- Dice/ConsoleService.cs
+++ Dice/ConsoleService.cs
@@ -9,14 +9,14 @@ namespace Dice
     {
 
         public string FilePath { get; set; }
-        public int NumberOfDice { get; set;  }
-        public int NumberOfSides { get; set; }
+        public int NumberOfDice { get; set; }
+        public int[] NumberOfSides { get; set; }
 
         public void ImportDataFile()
         {
             GetFilePath();
-            GetNumberOfDice();
-            GetNumberOfSides();
+            int num = GetNumberOfDice();
+            GetNumberOfSides(num);
         }
 
         public void GetFilePath()
@@ -34,23 +34,25 @@ namespace Dice
                     Console.WriteLine("Sorry that file path does not exist.");
                 }
             }
-            FilePath = filePath;           
+            FilePath = filePath;
         }
 
-        private void GetNumberOfDice()
-        {           
+        private int GetNumberOfDice()
+        {
             string input;
+            int returnNumber = 0;
             bool isValidNumber = false;
-            
+
             while (!isValidNumber)
             {
                 Console.WriteLine("How many dice rolls on each line?");
                 input = Console.ReadLine();
                 if (int.TryParse(input, out int number))
-                {                   
+                {
                     if (number > 0 && number <= 10)
                     {
                         NumberOfDice = number;
+                        returnNumber = number;
                         isValidNumber = true;
                     }
                 }
@@ -58,32 +60,94 @@ namespace Dice
                 {
                     Console.WriteLine("Please enter a number from 1 to 10.");
                 }
-            }            
+            }
+
+            return returnNumber;
         }
 
-        private void GetNumberOfSides()
+        private void GetNumberOfSides(int numDice)
         {
 
             string input;
-            bool isValidNumber = false;
+            NumberOfSides = new int[numDice];
 
-            while (!isValidNumber)
+            for (int i = 0; i < numDice; i++)
             {
-                Console.WriteLine("How many sides to each die?");
-                input = Console.ReadLine();
-                if (int.TryParse(input, out int number))
+                bool isValidNumber = false;
+                while (!isValidNumber)
                 {
-                    if (number > 0)
+                    Console.WriteLine("How many sides does die #" + i + " have?");
+                    input = Console.ReadLine();
+                    if (int.TryParse(input, out int number))
                     {
-                        NumberOfSides = number;
-                        isValidNumber = true;
+                        if (number > 0)
+                        {
+                            NumberOfSides[i] = number;
+                            isValidNumber = true;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter an integer greater than 0.");
                     }
                 }
-                else
-                {
-                    Console.WriteLine("Please enter an integer greater than 0.");
-                }
             }
+        }
+
+        public void PrintResults(Die[] inputDice)
+        {
+            Die[] dice = inputDice;
+            for (int i = 0; i < dice.Length; i++)
+            {
+                int sides = dice[i].GetNumberOfSides();
+                for (int j = 1; j <= sides; j++)
+                {
+                    double chiSquared = dice[i].ChiSquared(j);
+                    int faceTotal = dice[i].GetFaceTotal(j);
+                    Console.WriteLine(j + " total: " + faceTotal + " chi squared: " + chiSquared);
+                }
+                Console.WriteLine("Total Chi Squared: " + dice[i].TotalChiSquared());
+                Console.WriteLine("Average of all rolls: " + dice[i].Average());
+                Console.WriteLine();
+            }
+        }
+
+        public Die[] PopulateDiceData()
+        {
+            // create a Die bucket to hold each set of die results
+            Die[] dice = new Die[NumberOfDice];
+
+            StreamReader file = new StreamReader(FilePath);
+            string line;
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                Die die = new Die(NumberOfSides[i]);
+                dice[i] = die;
+            }
+
+            try
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    string[] data = line.Split(' ');
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        if (int.TryParse(data[i], out int number))
+                        {
+                            dice[i].AddRoll(number);
+                        }
+                    }
+                }
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return dice;
         }
 
         public void PrintResults(Die[] inputDice)
--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 # dice-chi
 This is a console application that
 accepts an input txt file of dice rolls and 
 totals the occurrences for each face, totals the average, and calculates the chi squared value and
 displays the results to the console.
 
+=======
+# dice-chi
+This is a console application that
+accepts an input txt file of dice rolls and 
+totals the occurrences for each face, totals the average, and calculates the chi squared value and
+displays the results to the console.
+
+>>>>>>> f4e45236cb1afdf97c95264fc0bb85b43b98b34b
 See the Data folder for examples of the input txt file format. Each roll is on a separate line. For multiple dice in the same file, each dice must have the same number of sides and separated by a space so that each die has its own column.
